258d763d11cef293372e35918d0804f308fb916b
Dave Sieh <dave.sieh@nursinghomequality.com>
2010-09-03 16:18:35 -0600

    Added stylesheets and jquery for date picker stuff

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,36 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(function(){
+
+	// Set up date picker fields
+	$('.date_picker').livequery(function(){
+		// Load required resources
+		if(!$.fn.datepicker) {
+			$.require('jquery/jqueryui.core.js');
+			$.require('jquery/jqueryui.datepicker.js');
+		}
+		$('.date_picker').datepicker({dateFormat: 'yy-mm-dd'});
+	});
+
+});
+
+/*
+	REQUIRE PLUGIN
+	-----------------------------------
+	if(!$.fn.skipLogic){$.require('jquery/jquery.skipLogic.js');}
+*/
+
+(function($){
+
+	$.require = function(resource){
+
+		$('<script/>')
+			.attr({
+				src : '/javascripts/'+resource,
+				type : 'text/javascript'
+			})
+			.appendTo('head');
+	};
+
+})(jQuery);
