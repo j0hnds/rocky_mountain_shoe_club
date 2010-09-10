931ff7d8247147bbcbe76fcec2c7f698623dd0c0
Dave Sieh <dave.sieh@nursinghomequality.com>
2010-09-10 17:36:24 -0600

    Cleaning up look/feel

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,8 @@ module ApplicationHelper
   def state_postal_codes
     %w{ AL AK AZ AR CA CO CT DE DC FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI WY }
   end
+
+  def menu_item_selected(controller_name)
+    ' selected' if controller.controller_name == controller_name
+  end
 end
