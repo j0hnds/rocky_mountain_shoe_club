baba3b6227a606bdd7c6263768f70754ba4fe86e
j0hnds@gmail.com <j0hnds@gmail.com>
2010-09-19 16:04:18 -0600

    Added princely reporting; set up the initial report 'exhibitor lines'

--- app/controllers/exhibitor_lines_controller.rb
+++ app/controllers/exhibitor_lines_controller.rb
@@ -1,3 +1,18 @@
 class ExhibitorLinesController < ApplicationController
 
+  def index
+    @exhibitor_lines = ExhibitorLine.for_show(@current_show.id).ordered
+    
+    tf = Tempfile.open('exhibitor_lines_report')
+    tf.write(make_pdf)
+    tf.close
+    
+    send_file tf.path,
+      :filename => 'exhibitor_list_by_line.pdf',
+      :type => 'application/pdf',
+      :disposition => 'inline',
+      :stream => false
+
+    tf.unlink
+  end
 end
--- app/models/exhibitor_line.rb
+++ app/models/exhibitor_line.rb
@@ -1,6 +1,11 @@
 class ExhibitorLine < ActiveRecord::Base
   belongs_to :exhibitor_registration
+  has_one :exhibitor, :through => :exhibitor_registration
+  has_one :show, :through => :exhibitor_registration
   
   validates_presence_of :exhibitor_registration_id, :line, :priority
   validates_length_of :line, :maximum => 80, :allow_blank => true
+
+  named_scope :ordered, :order => 'line ASC'
+  named_scope :for_show, lambda { | show_id | { :joins => :exhibitor_registration, :conditions => [ 'show_id = ?', show_id ] } }
 end
